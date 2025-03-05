90f0830cac215728a6b63f2ec85a6a88ed236173
Juan15377 <juan1092jesus543@gmail.com>
2025-03-04 22:07:47 -0600

    delete add 100 professors to tests

--- src/UI/database.py
+++ src/UI/database.py
@@ -3,6 +3,3 @@ from src.models.database import DataBaseManager
 from src.tests.database_example import database_example
 
 database = DataBaseManager()
-
-for i in range(1000):
-    database.professors.new(f"{i}")
